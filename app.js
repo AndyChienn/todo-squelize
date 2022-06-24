a12b2b3bf0dabd7a6b0d0c71ec87998293ac6ba2
AndyChienn <andy2002chien@hotmail.com.tw>
2022-06-24 11:52:17 +0800

    feat: add register function

--- app.js
+++ app.js
@@ -41,8 +41,29 @@ app.post('/users/login', (req, res) => {
   res.send('login')
 })
 
-app.get('/users/register', (req, res) => {
-  res.render('register')
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 
