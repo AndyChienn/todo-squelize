f99effbd31a7d4dd067f1ce6b4dbb4ad75b546d5
AndyChienn <andy2002chien@hotmail.com.tw>
2022-06-24 19:15:10 +0800

    feat: add passport and flash msg

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const bcrypt = require('bcryptjs')
 
 const PORT = 3000
@@ -30,8 +31,16 @@ app.use(methodOverride('_method'))
 
 
 usePassport(app)
+app.use(flash())
 
-
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.login_error = req.flash('error')
+  next()
+})
 
 
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,7 +15,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -24,10 +25,28 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位皆是必填的！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ where: { email } }).then(user => {
     if (user) {
+      errors.push({ message: '這個email已經註冊過！' })
       console.log('User already exists')
       return res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -51,7 +70,9 @@ router.post('/register', (req, res) => {
 
 
 router.get('/logout', (req, res) => {
-  res.send('logout')
+  req.logout()
+  req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
 })
 
 module.exports = router
